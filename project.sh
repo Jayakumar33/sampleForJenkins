60c680047dce1c82b6dc3f8e7713caefb411afc1
Jayakumar33 <118970708+Jayakumar33@users.noreply.github.com>
2022-12-13 13:36:40 +0530

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,1 +1,1 @@
-echo "This is my jenkins course. just creating this so i could familar with jenkins webhook and scm
+echo "This is my jenkins course. just creating this so i could familar with jenkins webhook and scm"
